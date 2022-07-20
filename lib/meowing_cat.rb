8214f70483e3462ce3317c5ce95e8d1f78582b99
Irene <irenemanningm@gmail.com>
2022-07-20 17:25:22 -0500

    commit tree d430f95037f9fd736ac07a7edb16073c6cfafc77

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,9 @@
-## code your solution here. 
+class Cat
+    attr_reader :name, :meow
+    attr_writer :name, :meow
+    attr_accessor :name, :meow
+    def meow
+        puts "meow!"
+    end
+    
+end
